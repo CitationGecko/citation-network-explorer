ac8c3eb1367dbb189de17e1c6fcbf2d68d0d245c
bjw49 <barneywalker49@gmail.com>
2018-01-13 13:09:26 +0000

    adding back-end javascript

--- geckoSkin/layout.js
+++ geckoSkin/layout.js
@@ -1,11 +1,53 @@
 //For threshold slider
 
-    function updateThreshold(val) {
-        document.querySelector('#thresholdOutput').value = val;
+    document.getElementById('thresholdInput').oninput = function(){
+
+        document.querySelector('#thresholdOutput').value = this.value;
+
+
     }
 
-//For addSeedModal
 
+//For Seed Papers Side bar
+
+    document.getElementById('collapseBar').onclick = hideSideBar;
+
+    function hideSideBar(){
+
+        document.getElementById('seedPapers').style.display='none';
+
+        var icon = document.getElementById('collapse-seeds-icon');
+
+        icon.classList.remove('fa-chevron-left');
+
+        icon.classList.add('fa-chevron-right');
+
+        document.getElementById('collapseBar').onclick = showSideBar;
+
+    }
+
+    function showSideBar(){
+
+        document.getElementById('seedPapers').style.display='block';
+
+        var icon = document.getElementById('collapse-seeds-icon');
+
+        icon.classList.remove('fa-chevron-right');
+
+        icon.classList.add('fa-chevron-left');
+
+        document.getElementById('collapseBar').onclick = hideSideBar;
+
+    }
+
+//For addSeedModals
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target.classList.contains('modal')) {
+            event.target.style.display = "none";
+        }
+    } 
 
     // When the user clicks on the button, open the modal
     document.getElementById("addSeeds").onclick = function() {
@@ -13,18 +55,32 @@
         document.getElementById('addSeedModal').style.display = "block";
     }
    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == document.getElementById('addSeedModal')) {
-            event.target.style.display = "none";
-        }
-    } 
+    document.getElementById("addbyDOI").onclick = function() {
 
-//For Seed Papers Side bar
-document.getElementById('collapseBar').onclick = hideSideBar;
+        document.getElementById('addSeedModal').style.display = "none";
+        document.getElementById('doiInputModal').style.display = "block";
+    }
 
-function hideSideBar(){
+    document.getElementById("doiInput").onkeydown = function(){
+        
+        if (event.keyCode == 13){addSeedFromDOI(doiQuery)}
 
-    document.getElementById('toCollapse').style.display='none';
+    }
 
-}
+    document.getElementById("searchByTitle").onclick = function() {
+
+        document.getElementById('addSeedModal').style.display = "none";
+        document.getElementById('titleInputModal').style.display = "block";
+    }
+
+    document.getElementById("titleInput").onkeydown = function(){
+        
+        if (event.keyCode == 13){}
+        
+    }
+
+    document.getElementById("uploadBibTex").onclick = function() {
+
+        document.getElementById('addSeedModal').style.display = "none";
+        document.getElementById('uploadBibTexModal').style.display = "block";
+    }
