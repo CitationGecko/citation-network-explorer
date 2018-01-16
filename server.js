5b056edecda3c5075e81a05fbd127556d3a29ef3
bjw49 <barneywalker49@gmail.com>
2018-01-16 23:24:36 +0000

    changed ports

--- server.js
+++ server.js
@@ -115,8 +115,8 @@ var server = http.createServer(function (req, res) {
 
 })
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
