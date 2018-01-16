3244c6d59f8a7ccc0a5089a9d4fc4d9ee2acb353
bjw49 <barneywalker49@gmail.com>
2018-01-16 22:23:42 +0000

    set up server for Open Shift

--- server.js
+++ server.js
@@ -113,13 +113,11 @@ var server = http.createServer(function (req, res) {
     }
     
 
-}).listen(3000)
+})
 
-
-/* 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
-}); */
+}); 
