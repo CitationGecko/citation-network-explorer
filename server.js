0ebc1d1252f21d9955c4d3b26ad5a42206bb6a43
Vot <vot@siliconfen.co>
2018-05-11 14:25:02 +0100

    App switched to express, exposing API

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var http = require('http');
+var express = require('express');
 var path = require('path');
 var request = require('request');
 var fs = require('fs');
+var _ = require('lodash');
+var dynamo = require('./lib/dynamo');
 
 try {
     var apikey = require('./public/apikeys.js');
@@ -13,114 +15,86 @@ var API_KEY = apikey.key;
 
 console.log(API_KEY)
 
-extensions = {
-    ".html" : "text/html",
-    ".css" : "text/css",
-    ".js" : "application/javascript",
-    ".png" : "image/png",
-    ".gif" : "image/gif",
-    ".jpg" : "image/jpeg",
-    ".bib" : "text/plain"
-  };
+var app = express();
 
+// main route - gets the html output to the page
+app.get('/', function (req, res) {
+  var filePath = __dirname + '/public/GeckoApp.html';
 
-  function getFile(filePath,res,mimeType){
-	//does the requested file exist?
-	fs.exists(filePath,function(exists){
-		//if it does...
-		if(exists){
-			//read the file, run the anonymous function
-			fs.readFile(filePath,function(err,contents){
-				if(!err){
-					//if there was no error
-					//send the contents with the default 200/ok header
-					res.writeHead(200,{
-						"Content-type" : mimeType,
-						"Content-Length" : contents.length
-					});
-					res.end(contents);
-				} else {
-					//for our own troubleshooting
-					console.dir(err);
-				};
-			});
-		} else {
-            //if the file doesn't exist send 404 error message
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.end('<p>PAGE NOTE FOUND<p>');
-
-		};
-	});
-};
-
-var server = http.createServer(function (req, res) {
-
-    if(req.method=='GET'){
-
-        var fileName = path.basename(req.url) || 'GeckoApp.html';
-        var ext = path.extname(fileName);
-        var localFolder = __dirname + '/public'+path.dirname(req.url);
-        var location = localFolder +'/'+ fileName;
-
-        if(!extensions[ext]){//check if file extension is in list of supported file types.
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.end("&lt;html&gt;&lt;head&gt;&lt;/head&gt;&lt;body&gt;The requested file type is not supported&lt;/body&gt;&lt;/html&gt;");
-        };
-
-        getFile(location,res,extensions[ext]);
-
+  fs.readFile(filePath, function (err, contents) {
+    if (err) {
+      return res.send('Something went wrong when reading the main html file');
     }
 
-    if(req.method =='OPTIONS'){
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': contents.length
+    });
+    res.end(contents.toString());
+  });
+});
 
-        res.writeHead(201, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-type', 'Access-Control-Allow-Methods': 'POST'
-    
-        });
-        res.end();
+app.use(express.static('public'));
 
+app.get('/api/v1/getCitedBy', function (req, res) {
+  var doi = req.query.doi;
+  if (!doi) {
+    return res.json({success: false, error: 'You need to specify the "doi" parameter.'});
+  }
+
+  dynamo.getThisArticleCitedBy(doi, function (err, data) {
+    if (err) {
+      return res.json({success: false, error: err});
+    }
+    var citations = _.get(data, 'Items', []);
+    var citationsMapped = _.map(citations, function (item) {
+      return {
+        citeFrom: _.get(item, 'citeFrom.S', ''),
+        citeTo: _.get(item, 'citeTo.S', '')
+      }
+    });
+    res.json(citationsMapped);
+  });
+});
+
+
+// maintaining this as a legacy endpoint - likely to be phased out shortly
+app.post('/', function (req, res) {
+  var query = '';
+
+  req.on('data', function (data) {
+    query += data;
+  });
+
+  req.on('end', function () {
+    console.log('Request recieved: ' + query);
+    // Set the headers
+    var headers = {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': API_KEY
+    }
+    // Configure the request
+    var options = {
+      url: 'https://westus.api.cognitive.microsoft.com/academic/v1.0/graph/search?mode=json',
+      method: 'POST',
+      headers: headers,
+      body: query
     }
 
-    if (req.method == 'POST') {
-        
-        var query = '';
+    // Start the request
+    request(options, function (error, response, body) {
+      console.log(JSON.stringify(response))
+      if (error) {
+        console.log(error.message)
+        return res.send('ERR');
+      }
 
-        req.on('data', function (data) {
-            query += data;
-        });
-
-
-        req.on('end', function () {
-            
-            console.log('Request recieved: '+ query)
-            // Set the headers
-            var headers = {
-                "Content-type": "application/json",
-                "Ocp-Apim-Subscription-Key": API_KEY
-            }
-            // Configure the request
-            var options = {
-                url: 'https://westus.api.cognitive.microsoft.com/academic/v1.0/graph/search?mode=json',
-                method: 'POST',
-                headers: headers,
-                body: query
-            }
-
-            // Start the request
-            request(options, function (error, response, body) {
-
-                console.log(JSON.stringify(response))
-                if(error) {console.log(error.message)}
-                if (!error) {
-                    res.writeHead(response.statusCode, {'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'});
-                    res.end(body.toString());
-                }
-            }) 
-        });
-    }
-    
-
+      res.writeHead(response.statusCode, {'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'});
+      res.end(body.toString());
+    })
+  });
 })
- 
-server.listen(3000, "localhost", function () {
-  console.log( "Listening on port 3000")
-}); 
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
+});
