e568181db22320d7a9d6638af2cebbcdbfbc15c6
Fábio Madeira <fbiomadeira@gmail.com>
2018-05-11 11:37:40 +0100

    Changed ingestion to use event-stream

--- dynamo/ingestData.js
+++ dynamo/ingestData.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var es = require('event-stream');
 var _ = require('lodash');
 var dynamo = require('../lib/dynamo');
 
@@ -24,45 +25,60 @@ function createDynamoPutRequest(citeFrom, citeTo) {
  * 3) runs batch inserts
  */
 function processInputData() {
-  var data = fs.readFileSync(__dirname + '/xaa.tsv').toString();
-  var dataByLine = data.split('\n');
-  var remainingLines = dataByLine.length;
-  console.log('total lines:', remainingLines);
+ var lineNr = 0;
+ var batch = [];
+ var batchIndex = 0;
 
-  var batch = [];
-  var batchIndex = 0;
+ var s = fs.createReadStream(__dirname + '/xaa.tsv')
+     .pipe(es.split('\n'))
+     .pipe(es.mapSync(function(line){
 
-  _.each(dataByLine, function (line) {
-    remainingLines--;
-    var currentLine = line.split('\t');
+         // pause the readstream
+         s.pause();
 
-    // var names indicate the direction of citation
-    // "citeTo" article is cited by "citeFrom"
-    var citeFrom = currentLine[0];
-    var citeTo = currentLine[1];
+         lineNr += 1;
 
-    if (typeof citeFrom === 'string' && typeof citeTo === 'string') {
-      batch.push(createDynamoPutRequest(citeFrom, citeTo));
-    }
+         // process line here and call s.resume() when rdy
+         var currentLine = line.split('\t');
 
-    // if batch is large enough or there's nothing more to process - insert
-    if (batch.length === 25 || remainingLines === 0) {
-      batchIndex++;
-      var currBatch = batch;
-      batch = [];
+         // var names indicate the direction of citation
+         // "citeTo" article is cited by "citeFrom"
+         var citeFrom = currentLine[0];
+         var citeTo = currentLine[1];
 
-      console.log('inserting batch #' + batchIndex + ', size: ' + currBatch.length);
-      // console.log(JSON.stringify(currBatch, null, 2));
+         if (typeof citeFrom === 'string' && typeof citeTo === 'string') {
+           batch.push(createDynamoPutRequest(citeFrom, citeTo));
+         }
 
-      if (currBatch.length) {
-        dynamo.batchInsert(currBatch, function (err, data) {
-          console.log('insert err', err);
-          console.log('insert data', data);
-        });
-      }
-    }
+         // if batch is large enough or there's nothing more to process - insert
+         if (batch.length === 25 ) {
+            pushData(batch);
+            batchIndex++;
+            console.log(batchIndex);
+            // console.log(JSON.stringify(batch, null, 2));
+            batch = [];
+         }
 
-  });
+         // resume the readstream, possibly from a callback
+         s.resume();
+     })
+     .on('error', function(err){
+         console.log('Error while reading file.', err);
+     })
+     .on('end', function(){
+         pushData(batch);
+         console.log('Read entire file.')
+     })
+ );
+}
+
+function pushData(currBatch) {
+  if (currBatch.length) {
+    dynamo.batchInsert(currBatch, function (err, data) {
+      // console.log('insert err', err);
+      // console.log('insert data', data);
+    });
+  }
 }
 
 processInputData();
