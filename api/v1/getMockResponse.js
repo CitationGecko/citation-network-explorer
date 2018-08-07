df70cd31106d56bbf302f11a66f799ff42abcfff
Vot <vot@siliconfen.co>
2018-08-07 18:50:33 +0100

    Updated the second paper and object in edges

--- api/v1/getMockResponse.js
+++ api/v1/getMockResponse.js
@@ -18,7 +18,7 @@ const paper1 = {
     }
   ],
   pubDate: "2012-08-17T00:00:00.000Z",
-  journal: "Science", // maybe parentProduct ?
+  journal: "Science",
   publisher: "American Association for the Advancement of Science",
 
   references: ["f15df7ca1eb0466f8f6ee50c8115b413", "10def5c61eb0466f8f6ee50c8115b882"],
@@ -28,32 +28,47 @@ const paper1 = {
 };
 
 const paper2 = {
-  id: "d01ff5c61eb0466f8f6ee50c8115b409",
-  doi: "10.1126/science.1222901",
-  microsoftId: "1234567890",
+  id: "10def5c61eb0466f8f6ee50c8115b882",
+  doi: "10.1111/j.1365-2958.2009.07028.x",
+  microsoftId: "6789012345",
 
   isSeed: true,
-  title: "Type 6 Secretion Dynamics Within and Between Bacterial Cells",
+  title: "The SciZ protein anchors the enteroaggregative Escherichia coli Type VI secretion system to the cell wall",
   authors: [
     {
-      firstName: "M.",
-      lastName: "Basler",
+      firstName: "Marie‐Stéphanie",
+      lastName: "Aschtgen",
       orcid: "123456"
     },
     {
-      firstName: "J. J.",
-      lastName: "Mekalanos",
+      firstName: "Marthe",
+      lastName: "Gavioli",
+      orcid: "654321"
+    }
+    {
+      firstName: "Andrea",
+      lastName: "Dessen",
+      orcid: "654321"
+    }
+    {
+      firstName: "Roland",
+      lastName: "Lloubès",
+      orcid: "654321"
+    }
+    {
+      firstName: "Eric",
+      lastName: "Cascales",
       orcid: "654321"
     }
   ],
   pubDate: "2012-08-17T00:00:00.000Z",
-  journal: "Science", // maybe parentProduct ?
-  publisher: "American Association for the Advancement of Science",
+  journal: "Molecular Macrobiology",
+  publisher: "Wiley",
 
-  references: ["f15df7ca1eb0466f8f6ee50c8115b413", "10def5c61eb0466f8f6ee50c8115b882"],
-  citationCount: 20,
-  openaccess: false,
-  url: "http://science.sciencemag.org/content/337/6096/815"
+  references: ["f15df7ca1eb0466f8f6ee50c8115b413", "d01ff5c61eb0466f8f6ee50c8115b409"],
+  citationCount: 64,
+  openaccess: true,
+  url: "https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1365-2958.2009.07028.x"
 };
 
 module.exports = function (req, res) {
@@ -66,8 +81,8 @@ module.exports = function (req, res) {
     papers: [paper1, paper2],
     edges: [
       {
-        source: paper1,
-        target: paper2
+        source: 'd01ff5c61eb0466f8f6ee50c8115b409',
+        target: '10def5c61eb0466f8f6ee50c8115b882'
       }
     ]
   };
