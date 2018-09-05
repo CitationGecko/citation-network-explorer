301dabb87ade08e04db3989297b739bf13df512b
Barney <barneywalker49@gmail.com>
2018-09-05 17:41:43 +0100

    refactor network view

--- src/ui/visualisations/network-view/network-view.js
+++ src/ui/visualisations/network-view/network-view.js
@@ -2,184 +2,186 @@ import { eventResponse, Edges, Papers , updateMetrics } from "core";
 import { updateInfoBox } from 'ui/visualisations/info-box'
 import * as d3 from 'vendor/d3.v4.js' 
 
-eventResponse(false,'newSeed',function(){
+eventResponse(true,'newSeed',function(){
     updateMetrics(Papers,Edges); // update citation metrics
     forceGraph.refresh()
 })
-eventResponse(false,'newEdges',function(){
+eventResponse(true,'newEdges',function(){
     updateMetrics(Papers,Edges); // update citation metrics
     forceGraph.refresh()
 })
-eventResponse(false,'seedDeleted',function(){
+eventResponse(true,'seedDeleted',function(){
     updateMetrics(Papers,Edges); // update citation metrics
     forceGraph.refresh()
 })
 
-export const forceGraph = {
-
-    refresh: function(){                
-        //Pick only edges that you want to display i.e. citedBy vs references
-        switch(forceGraph.mode){     
-            case 'ref':
-            forceGraph.edges = Edges.filter(function(e){return(e.source.seed)}).map(function(e){return {source: e.source.ID, target: e.target.ID}});
-            forceGraph.sizeMetric = 'seedsCitedBy';
-            break;
-            case 'citedBy':
-            forceGraph.edges = Edges.filter(function(e){return(e.target.seed)}).map(function(e){return {source: e.source.ID, target: e.target.ID}});       
-            forceGraph.sizeMetric = 'seedsCited';    
-            break;
-        }
-        forceGraph.nodeIDs = forceGraph.edges.map(function(e){return(e.source)}).concat(forceGraph.edges.map(function(e){return(e.target)}));
-        //Pick only Papers that are connected to something
-        forceGraph.nodes = Papers.filter(function(p){
-            return(forceGraph.nodeIDs.includes(p.ID)||p.seed)
-        }); 
-        forceGraph.circles = forceGraph.circles.data(forceGraph.nodes,function(d){return d.ID});
-        forceGraph.circles.exit().remove();
-        forceGraph.circles = forceGraph.circles.enter().append("circle")
-                            .merge(forceGraph.circles)
-                            .attr("r", function(d){return d.seed ? 10 : 5*d[forceGraph.sizeMetric]})
-                            .attr("class", function(d) { 
-                                if(d.seed){return 'seed-node node'} else {return 'node'}
-                            })                                            
-                            .style("visibility", function (d) {return d.hide == 1 ? "hidden" : "visible";})
-                            .call(d3.drag()
-                                .on("start", forceGraph.dragstarted)
-                                .on("drag", forceGraph.dragged)
-                                .on("end", forceGraph.dragended))
-                            .on("dblclick",forceGraph.hideSingles)
-                            .on("click",p=>forceGraph.highlightNode(p))
-                            .on("mouseover",p=>updateInfoBox(p))
-        forceGraph.circles.append("title").text(function(d) { return d.title; }); //Label nodes with title on hover
-        forceGraph.lines = forceGraph.lines.data(forceGraph.edges, function(d) { return d.source.ID + "-" + d.target.ID; })
-        forceGraph.lines.exit().remove();
-        forceGraph.lines = forceGraph.lines.enter().append("line").attr("marker-end", "url(#end)").merge(forceGraph.lines);
-        // Update and restart the simulation.
-        forceGraph.simulation.nodes(forceGraph.nodes).on("tick", forceGraph.ticked);
-        forceGraph.simulation.force("link").links(forceGraph.edges);
-        forceGraph.simulation.force("collide").initialize(forceGraph.simulation.nodes());
-        forceGraph.simulation.alpha(1).restart();
-        forceGraph.threshold(forceGraph.minconnections);   
-        forceGraph.circles.style("opacity", 1);
-        forceGraph.lines.style("opacity",1);   
-    },   
-    dragstarted:  function(d) {
-        if (!d3.event.active) forceGraph.simulation.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
-    },
-    dragged:  function(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-    },
-    dragended:  function(d) {
-        if (!d3.event.active) forceGraph.simulation.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
-    },
-    hideSingles:  function(){
-        let nodeid = this.__data__.ID;
-        childrenids = findUniqueChildren(nodeid);
-        Papers.filter(function(p){return childrenids.includes(p.ID)}).forEach(function(p){p.hide= !p.hide;});
-        Edges.filter(function(e){
-            let hiddenPapers = Papers.filter(function(p){return p.hide}).map(function(p){return p.ID});
-            return hiddenPapers.includes(e.source.ID) | hiddenPapers.includes(e.target.ID);
-        }).forEach(function(e){
-            e.hide=true
-        })
-        forceGraph.circles.style("visibility", function (p) {
-            return p.hide ? "hidden" : "visible" ;
-        });
-        forceGraph.lines.style("visibility", function(e){
-            var hiddenPapers = Papers.filter(function(p){return p.hide}).map(function(p){return p.ID});
-            return hiddenPapers.includes(e.source.ID) | hiddenPapers.includes(e.target.ID) ? "hidden":"visible";
-        })  
-    },
-    neighboring:  function(a, b) {
-        return (
-            forceGraph.edges.filter(function(e){
-                return e.source == a | e.target == a
-            }).filter(function(e){
-                return e.source == b | e.target == b
-            }).length
-        )
-    },
-    highlightNode: function(d){
-        forceGraph.circles.style("opacity", 1);
-        forceGraph.lines.style("opacity",1);
-        forceGraph.circles.style("opacity", function (o) {
-            return forceGraph.neighboring(d, o) | forceGraph.neighboring(o, d) ? 1 : 0.15;
-        });
-        forceGraph.lines.style("opacity", function(o) {
-            return o.source === d || o.target === d ? 1 : 0.15;
-        });
-        updateInfoBox(d);
-        forceGraph.circles.on('mouseover',null)
-        d3.event.stopPropagation();
-    },
-    ticked: function() {
-        
-            forceGraph.lines
-                .attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
-    
-            forceGraph.circles
-                .attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
-    },
-    threshold:  function(value){
-        let metric;
-        switch(forceGraph.mode){
-            case 'ref':
-                metric = 'seedsCitedBy';
-                break;
-            case 'citedBy':
-                metric = 'seedsCited';
-                break;
-        } 
-        Papers.forEach(function(p){
-            p.hide = (p[metric]>=value || p.seed) ? false : true ;
-        });
-        forceGraph.circles.style("visibility", function (p) {
-            return p.hide ? "hidden" : "visible" ;
-        });
-        var hiddenPapers = Papers.filter(function(p){return p.hide}).map(function(p){return p.ID});        
-        forceGraph.lines.style("visibility", function(e){     
-            return hiddenPapers.includes(e.source.ID) | hiddenPapers.includes(e.target.ID) ? "hidden":"visible";  
-        })
-    }
-}
+export const forceGraph = {};
 
 forceGraph.minconnections = 0;
 forceGraph.mode = 'ref';
 forceGraph.sizeMetric = 'seedsCitedBy';
 forceGraph.selectednode = null;
 forceGraph.osvg =  d3.select('#force-graph')
-                        .on('click',function(){
-                            forceGraph.circles.style("opacity", 1);
-                            forceGraph.lines.style("opacity",1);
-                            forceGraph.circles.on('mouseover',p=>updateInfoBox(p))
-                            d3.selectAll('.paper-box').classed('selected-paper',false)
-                            document.getElementById('selected-paper-box').style.display ='none';
-                        })
-                        .call(d3.zoom().on("zoom", function () {forceGraph.svg.attr("transform", d3.event.transform)}))//enable zoom by scrolling
-                        .on("dblclick.zoom", null);//disable double click zooming
+    .on('click',function(){
+        forceGraph.circles.style("opacity", 1);
+        forceGraph.lines.style("opacity",1);
+        forceGraph.circles.on('mouseover',p=>updateInfoBox(p))
+        d3.selectAll('.paper-box').classed('selected-paper',false)
+        document.getElementById('selected-paper-box').style.display ='none';
+    })
+    .call(d3.zoom().on("zoom", function () {forceGraph.svg.attr("transform", d3.event.transform)}))//enable zoom by scrolling
+    .on("dblclick.zoom", null);//disable double click zooming
 forceGraph.width =  document.getElementById('network-view').offsetWidth; //extract the width and height attribute (the + converts to number)
 forceGraph.height =  document.getElementById('network-view').offsetHeight;
 forceGraph.svg =  forceGraph.osvg.append('g');
 forceGraph.lines =  forceGraph.svg.append("g").attr("class", "link").selectAll("line");
 forceGraph.circles =  forceGraph.svg.append("g").attr("class", "node").selectAll("circle");
 forceGraph.simulation =  d3.forceSimulation()
-                            .force("link", d3.forceLink().id(function(d){return d.ID;}))
-                            .force("charge", d3.forceManyBody().strength(-100))
-                            .force("center", d3.forceCenter(forceGraph.width / 2, forceGraph.height / 2))
-                            .force("xattract",d3.forceX())
-                            .force("yattract",d3.forceY())
-                            .force("collide",d3.forceCollide().radius(function(d){return (d.seed ? 7 : 5*d[forceGraph.sizeMetric])}));
-                    
+    .force("link", d3.forceLink().id(function(d){return d.ID;}))
+    .force("charge", d3.forceManyBody().strength(-100))
+    .force("center", d3.forceCenter(forceGraph.width / 2, forceGraph.height / 2))
+    .force("xattract",d3.forceX())
+    .force("yattract",d3.forceY())
+    .force("collide",d3.forceCollide().radius(function(d){return (d.seed ? 7 : 5*d[forceGraph.sizeMetric])}));
 
+forceGraph.refresh = function(){                
+    //Pick only edges that you want to display i.e. citedBy vs references
+    switch(forceGraph.mode){     
+        case 'ref':
+        forceGraph.edges = Edges.filter(function(e){return(e.source.seed)}).map(function(e){return {source: e.source.ID, target: e.target.ID}});
+        forceGraph.sizeMetric = 'seedsCitedBy';
+        break;
+        case 'citedBy':
+        forceGraph.edges = Edges.filter(function(e){return(e.target.seed)}).map(function(e){return {source: e.source.ID, target: e.target.ID}});       
+        forceGraph.sizeMetric = 'seedsCited';    
+        break;
+    }
+    forceGraph.nodeIDs = forceGraph.edges.map(function(e){return(e.source)}).concat(forceGraph.edges.map(function(e){return(e.target)}));
+    //Pick only Papers that are connected to something
+    forceGraph.nodes = Papers.filter(function(p){
+        return(forceGraph.nodeIDs.includes(p.ID)||p.seed)
+    }); 
+    forceGraph.circles = forceGraph.circles.data(forceGraph.nodes,function(d){return d.ID});
+    forceGraph.circles.exit().remove();
+    forceGraph.circles = forceGraph.circles.enter().append("circle")
+        .merge(forceGraph.circles)
+        .attr("r", function(d){return d.seed ? 10 : 5*d[forceGraph.sizeMetric]})
+        .attr("class", function(d) { 
+            if(d.seed){return 'seed-node node'} else {return 'node'}
+        })                                            
+        .style("visibility", function (d) {return d.hide == 1 ? "hidden" : "visible";})
+        .call(d3.drag()
+            .on("start", (d)=>dragstarted(d,forceGraph.simulation))
+            .on("drag", (d)=>dragged(d))
+            .on("end", (d)=>dragended(d,forceGraph.simulation)))
+        .on("dblclick",p=>hideSingles(p))
+        .on("click",p=>highlightNode(p))
+        .on("mouseover",p=>updateInfoBox(p))
+        .append("title").text(function(d) { return d.title; }); //Label nodes with title on hover
+
+    forceGraph.lines = forceGraph.lines.data(forceGraph.edges, function(d) { return d.source.ID + "-" + d.target.ID; })
+    forceGraph.lines.exit().remove();
+    forceGraph.lines = forceGraph.lines.enter().append("line").attr("marker-end", "url(#end)").merge(forceGraph.lines);
+    // Update and restart the simulation.
+    forceGraph.simulation.nodes(forceGraph.nodes).on("tick", ()=>tick(forceGraph));
+    forceGraph.simulation.force("link").links(forceGraph.edges);
+    forceGraph.simulation.force("collide").initialize(forceGraph.simulation.nodes());
+    forceGraph.simulation.alpha(1).restart();
+    threshold(forceGraph.minconnections);   
+    forceGraph.circles.style("opacity", 1);
+    forceGraph.lines.style("opacity",1);   
+}  
+
+function dragstarted(d,simulation) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+};
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+};
+
+function dragended(d,simulation) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+};
+
+function hideSingles(d){
+    let nodeid = d.ID;
+    childrenids = findUniqueChildren(nodeid);
+    Papers.filter(function(p){return childrenids.includes(p.ID)}).forEach(function(p){p.hide= !p.hide;});
+    Edges.filter(function(e){
+        let hiddenPapers = Papers.filter(function(p){return p.hide}).map(function(p){return p.ID});
+        return hiddenPapers.includes(e.source.ID) | hiddenPapers.includes(e.target.ID);
+    }).forEach(function(e){
+        e.hide=true
+    })
+    forceGraph.circles.style("visibility", function (p) {
+        return p.hide ? "hidden" : "visible" ;
+    });
+    forceGraph.lines.style("visibility", function(e){
+        var hiddenPapers = Papers.filter(function(p){return p.hide}).map(function(p){return p.ID});
+        return hiddenPapers.includes(e.source.ID) | hiddenPapers.includes(e.target.ID) ? "hidden":"visible";
+    })  
+};
+function neighboring(a, b, edges) {
+    return (
+        edges.filter(function(e){
+            return e.source == a | e.target == a
+        }).filter(function(e){
+            return e.source == b | e.target == b
+        }).length
+    )
+};
+
+function highlightNode(d){
+    forceGraph.circles.style("opacity", 1);
+    forceGraph.lines.style("opacity",1);
+    forceGraph.circles.style("opacity", function (o) {
+        return neighboring(d, o,forceGraph.edges) | neighboring(o, d,forceGraph.edges) ? 1 : 0.15;
+    });
+    forceGraph.lines.style("opacity", function(o) {
+        return o.source === d || o.target === d ? 1 : 0.15;
+    });
+    updateInfoBox(d);
+    forceGraph.circles.on('mouseover',null)
+    d3.event.stopPropagation();
+};
+
+function tick(graph){
+    graph.lines
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+    graph.circles
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+};
+
+function threshold(value){
+    let metric;
+    switch(forceGraph.mode){
+        case 'ref':
+            metric = 'seedsCitedBy';
+            break;
+        case 'citedBy':
+            metric = 'seedsCited';
+            break;
+    } 
+    Papers.forEach(function(p){
+        p.hide = (p[metric]>=value || p.seed) ? false : true ;
+    });
+    forceGraph.circles.style("visibility", function (p) {
+        return p.hide ? "hidden" : "visible" ;
+    });
+    var hiddenPapers = Papers.filter(function(p){return p.hide}).map(function(p){return p.ID});        
+    forceGraph.lines.style("visibility", function(e){     
+        return hiddenPapers.includes(e.source.ID) | hiddenPapers.includes(e.target.ID) ? "hidden":"visible";  
+    })
+}
 
 
 
--- src/ui/visualisations/timeline-view/timeline-view.js
+++ src/ui/visualisations/timeline-view/timeline-view.js
@@ -1,87 +1,81 @@
-import { eventResponse, Edges, Papers , updateMetrics } from "core";
+/* import { eventResponse, Edges, Papers , updateMetrics } from "core";
 import { updateInfoBox } from 'ui/visualisations/info-box'
 import * as d3 from 'vendor/d3.v4.js' 
 
-eventResponse(true,'seedUpdate',function(){
+var listening = false;
+
+eventResponse(listening,'seedUpdate',function(){
     updateMetrics(Papers,Edges); // update citation metrics
     timeline.refresh()
 })
-eventResponse(true,'newEdges',function(){
+eventResponse(listening,'newEdges',function(){
     updateMetrics(Papers,Edges); // update citation metrics
     timeline.refresh()
 })
-eventResponse(true,'seedDeleted',function(){
+eventResponse(listening,'seedDeleted',function(){
     updateMetrics(Papers,Edges); // update citation metrics
     timeline.refresh()
 })
+
+var width =  document.getElementById('timeline-view').offsetWidth; //extract the width and height attribute (the + converts to number)
+var height =  document.getElementById('timeline-view').offsetHeight;
+const svg = d3.select('#timeline-graph')
+    .on('click',function(){
+        circles.style("opacity", 1);
+        lines.style("opacity",1);
+        circles.on('mouseover',p=>updateInfoBox(p))
+        d3.selectAll('.paper-box').classed('selected-paper',false)
+        document.getElementById('selected-paper-box').style.display ='none';
+    })
+
+var lines =  svg.append("g").selectAll("line");
+var circles =  svg.append("g").selectAll("circle");
 
 export const timeline = {}
 
 timeline.minconnections = 0;
 timeline.sizeMetric = 'seedsCitedBy';
-timeline.selectednode = null;
-timeline.osvg = d3.select('#timeline-graph')
-                    .on('click',function(){
-                        timeline.circles.style("opacity", 1);
-                        timeline.lines.style("opacity",1);
-                        timeline.circles.on('mouseover',p=>updateInfoBox(p))
-                        d3.selectAll('.paper-box').classed('selected-paper',false)
-                        document.getElementById('selected-paper-box').style.display ='none';
-                    })
-timeline.width =  document.getElementById('timeline-view').offsetWidth; //extract the width and height attribute (the + converts to number)
-timeline.height =  document.getElementById('timeline-view').offsetHeight;
-timeline.svg =  timeline.osvg.append('g');
-timeline.lines =  timeline.svg.append("g").selectAll("line");
-timeline.circles =  timeline.svg.append("g").selectAll("circle");
-                    
+timeline.selectednode = null;        
 timeline.refresh = function(){                
     //Pick only edges that you want to display i.e. citedBy vs references
     timeline.edges = Edges.map(e=>{return {source:e.source.ID,target:e.target.ID}});
-    //Pick only Papers that are connected to something
+    //Pick only Papers that have a timestamp
     timeline.nodes = Papers.filter(function(p){
         return(p.timestamp)
     }); 
-
-    getY(timeline.nodes)
-    
-    timeline.circles
+    calculatePositions(nodes)
+    // Data bind step
+    circles = svg.selectAll('circles').data(nodes,function(d){return d.ID});
+    // Remove nodes that no longer exist.
+    circles.exit().remove();
+    // Update radius
+    circles.attr("r", function(d){return d.seed ? 10 : 5*Math.max(d.seedsCited,d.seedsCitedBy)})
+    // Update x position
+    circles.attr("cx",function(d){return(width*0.5)})
+    // Update y position
+    circles.attr("cy",function(d){return(5*(height-50)*d.yPos + 50)})
+    // Create new circles for new nodes
+    circles.enter().append("circle")
+        .merge(circles)
         .attr("r", function(d){return d.seed ? 10 : 5*Math.max(d.seedsCited,d.seedsCitedBy)})
         .attr("cx",function(d){
-        return(timeline.width*0.5)
+            return(5*(width-50)*d.yPos + 50)
         })
         .attr("cy",function(d){
-            return(5*(timeline.height-50)*d.yPos + 50)
-        })
-
-    timeline.circles = timeline.circles.data(timeline.nodes,function(d){return d.ID});
-    timeline.circles.exit().remove();
-    timeline.circles.enter().append("circle")
-        .merge(timeline.circles)
-        .attr("r", function(d){return d.seed ? 10 : 5*Math.max(d.seedsCited,d.seedsCitedBy)})
-        .attr("cx",function(d){
-            return(5*(timeline.width-50)*d.yPos + 50)
-        })
-        .attr("cy",function(d){
-            return(timeline.height*0.5)
+            return(height*0.5)
         })
         .attr("class", function(d){ return (d.seed ? 'seed-node node':'node')})                                            
         .style("visibility", function (d) {return d.hide == 1 ? "hidden" : "visible";})
-        .on("click",p=>timeline.highlightNode(p))
+        .on("click",p=>highlightNode(p))
         .on("mouseover",p=>updateInfoBox(p))
         .append("title").text(function(d) { return d.title; });  //Label nodes with title on hover        
 
-    /* timeline.lines = timeline.lines.data(timeline.edges, function(d) { return d.source.ID + "-" + d.target.ID; })
-    timeline.lines.exit().remove();
-    timeline.lines = timeline.lines.enter().append("line").attr("marker-end", "url(#end)").merge(timeline.lines);
-
-    timeline.threshold(timeline.minconnections);    */
-    timeline.circles.style("opacity", 1);
-    //timeline.lines.style("opacity",1);   
+    circles.style("opacity", 1);
 };
 
-timeline.neighboring = function(a, b) {
+function neighboring(a, b) {
     return (
-        timeline.edges.filter(function(e){
+        edges.filter(function(e){
             return e.source == a | e.target == a
         }).filter(function(e){
             return e.source == b | e.target == b
@@ -89,23 +83,23 @@ timeline.neighboring = function(a, b) {
     )
 };
 
-timeline.highlightNode = function(d){
-    timeline.circles.style("opacity", 1);
-    timeline.lines.style("opacity",1);
-    timeline.circles.style("opacity", function (o) {
-        return timeline.neighboring(d, o) | timeline.neighboring(o, d) ? 1 : 0.15;
+function highlightNode(d){
+    circles.style("opacity", 1);
+    lines.style("opacity",1);
+    circles.style("opacity", function (o) {
+        return neighboring(d, o) | neighboring(o, d) ? 1 : 0.15;
     });
-    timeline.lines.style("opacity", function(o) {
+    lines.style("opacity", function(o) {
         return o.source === d || o.target === d ? 1 : 0.15;
     });
     updateInfoBox(d);
-    timeline.circles.on('mouseover',null)
+    circles.on('mouseover',null)
     d3.event.stopPropagation();
 };
 
-timeline.threshold = function(value){
+function threshold(value){
     let metric;
-    switch(timeline.mode){
+    switch(mode){
         case 'ref':
             metric = 'seedsCitedBy';
             break;
@@ -116,16 +110,16 @@ timeline.threshold = function(value){
     Papers.forEach(function(p){
         p.hide = (p[metric]>=value || p.seed) ? false : true ;
     });
-    timeline.circles.style("visibility", function (p) {
+    circles.style("visibility", function (p) {
         return p.hide ? "hidden" : "visible" ;
     });
     var hiddenPapers = Papers.filter(function(p){return p.hide}).map(function(p){return p.ID});        
-    timeline.lines.style("visibility", function(e){     
+    lines.style("visibility", function(e){     
         return hiddenPapers.includes(e.source.ID) | hiddenPapers.includes(e.target.ID) ? "hidden":"visible";  
     })
 }
 
-function getY(nodes){
+function calculatePositions(nodes){
   let pos = [];
   nodes.sort(function(a, b) {
   	return a.timestamp - b.timestamp;
@@ -144,4 +138,4 @@ function getY(nodes){
   }
 }
 
-  
+   */
