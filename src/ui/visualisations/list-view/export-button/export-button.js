49b30f9c2f8b1010a3a8f6588023877a7bfde2a2
bjw49 <barneywalker49@gmail.com>
2018-09-07 07:53:46 +0100

    merge with zotero-export branch

--- src/ui/visualisations/list-view/export-button/export-button.js
+++ src/ui/visualisations/list-view/export-button/export-button.js
@@ -2,7 +2,10 @@ import * as d3 from 'vendor/d3.v4.js'
 import { addItems } from 'integrations/zotero'
 
 d3.select('#export-button').on('click',()=>{
+    document.getElementById('coming-soon').style.display = "block";
+})
+/* d3.select('#export-button').on('click',()=>{
     var selectedPapers = d3.selectAll('.selected-paper').data()
     addItems(selectedPapers);
 })
-
+ */
